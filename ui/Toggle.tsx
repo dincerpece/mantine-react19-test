214886e343f69079ed2e1704808a59b1279fe30a
dincer <dincerpece@gmail.com>
2024-12-10 03:36:41 +0300

    Remove .idea folder from version control

---
+++ ui/Toggle.tsx
@@ -0,0 +1,17 @@
+import { Menu } from '@mantine/core'
+import React from 'react'
+
+export default function Toggle({ trager, content }: { trager: React.ReactNode; content: React.ReactNode }) {
+  return (
+    <Menu
+      shadow="md"
+      width={200}
+      transitionProps={{ transition: 'pop-top-right' }}
+      // position="top-end"
+      withinPortal
+    >
+      <Menu.Target>{trager}</Menu.Target>
+      <Menu.Dropdown className={'dark:bg-black3 bg-white1 shadow-dpBoxShadow3 border-none z-[10000]'}>{content}</Menu.Dropdown>
+    </Menu>
+  )
+}
